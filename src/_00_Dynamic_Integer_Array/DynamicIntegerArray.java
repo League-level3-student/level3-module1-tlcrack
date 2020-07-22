78f1ae22abc5ba81e10bf8f28be332d9e712594c
root <root@8d21d1874d85>
2020-07-22 01:25:24 +0000

    Guest book in progress

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -1,95 +1,117 @@
 package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
-	//1. Create a private int array. Don't initialize it.
+	// 1. Create a private int array. Don't initialize it.
+	private int[] ints;
 
-	
 	public DynamicIntegerArray() {
-		//2. Initialize the int array to have 0 elements. 
-		//   This will prevent a null pointer exception
-	
+		// 2. Initialize the int array to have 0 elements.
+		// This will prevent a null pointer exception
+		ints = new int[0];
 	}
-	
-	//3. Complete the steps in the add method
+
+	// 3. Complete the steps in the add method
 	public void add(int v) {
-		//A. create and initialize a new int array to be one 
-		//   element longer than the member array
-		
-		//B. set the last element of your new array to 
-		//   the value passed into the method
-		
-		//C. iterate through the member array and 
-		//   copy every element from the member array 
-		//   to the new array
-		
-		//D. set the member array equal to the new array.
+		// A. create and initialize a new int array to be one
+		// element longer than the member array
+		int[] newInts = new int[ints.length + 1];
+		// B. set the last element of your new array to
+		// the value passed into the method
+		newInts[newInts.length - 1] = v;
+		// C. iterate through the member array and
+		// copy every element from the member array
+		// to the new array
+		for (int i = 0; i < ints.length; i++) {
+			newInts[i] = ints[i];
+		}
+		// D. set the member array equal to the new array.
+		ints = newInts;
 	}
-	
-	//4. Complete the steps in the get method
+
+	// 4. Complete the steps in the get method
 	public int get(int location) {
-		//A. Return the value of the memeber array at the location passed in
-		return 0;
+		// A. Return the value of the memeber array at the location passed in
+		int returned = ints[location];
+		return returned;
 	}
-	
-	//5. Run the DynamicArrayTest to see if you are correct so far.
-	
-	
-	//6. Complete the steps in the set method
+
+	// 5. Run the DynamicArrayTest to see if you are correct so far.
+
+	// 6. Complete the steps in the set method
 	public void set(int v, int location) {
-		//A. set the variable at the location passed in to the method
-		//   to the new value v
+		// A. set the variable at the location passed in to the method
+		// to the new value v
+		ints[location] = v;
 	}
-	
-	//7. Complete the steps in the insert method
+
+	// 7. Complete the steps in the insert method
 	public void insert(int v, int location) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] newerInts = new int[ints.length+1];
 		//B. Make a for loop that iterates through the new array
-
+		for(int i = 0; i < newerInts.length; i++) {
 			//C. if i is less than location:
 		    //		set the element at i of the new array to the element at i of the member array
-			
+			if(i<location) {
+				newerInts[i] = ints[i];
+			}
 			//D. else if i is greater than location:
 		    //		set the element at i of the new array to the i - 1 element of the member array
-			
+			else if(i>location) {
+				newerInts[i] = ints[i-1];
+			}
 			//E. else, set the element at i of the new array to the value v
-		
-		
+			else {
+				newerInts[i]=v;
+			}
+		}		
 		//F. set the member array equal to the new array
-	
+	ints=newerInts;
 	}
-	
-	//8. Run the tests again and check your progress
-	
-	//9. Complete the steps in the remove method
+
+	// 8. Run the tests again and check your progress
+
+	// 9. Complete the steps in the remove method
 	public void remove(int location) {
-		//A. create a new array that is one element smaller than the member array
-		
-		//B. make a for loop to iterate through the member array
-		
-			//C. if i  is less than location
-			//		set the element at i of the new array to the element at i of the member array
-			
-			//D. else if i  is greater than location
-			//		set the element at i - 1 of the new array to the element at i of the member array
-			
-			//E. else, continue;
-			
-		//F. set the member array equal to the new array
+		// A. create a new array that is one element smaller than the member array
+		int[] evenNewerInts = new int[ints.length-1];
+		// B. make a for loop to iterate through the member array
+		for(int i = 0; i < ints.length; i++) {
+		// C. if i is less than location
+		// set the element at i of the new array to the element at i of the member array
+			if(i<location) {
+				evenNewerInts[i]=ints[i];
+			}
+		// D. else if i is greater than location
+		// set the element at i - 1 of the new array to the element at i of the member
+		// array
+			else if(i>location) {
+				evenNewerInts[i-1] = ints[i];
+			}
+		// E. else, continue;
+			else {
+				continue;
+			}
+		}
+		// F. set the member array equal to the new array
+	ints=evenNewerInts;
 	}
-	
-	//10. Run the tests again the see if you are correct so far
-	
-	//11. Complete the size method so that it returns the length of the member array.
+
+	// 10. Run the tests again the see if you are correct so far
+
+	// 11. Complete the size method so that it returns the length of the member
+	// array.
 	public int size() {
-		return 0;
+		return ints.length;
 	}
-	
-	//12. Complete the clear array so that it sets the member array 
-	//    equal to a new integer array of size 0
+
+	// 12. Complete the clear array so that it sets the member array
+	// equal to a new integer array of size 0
 	public void clear() {
+		int[] newestInts = new int[0];
+		ints=newestInts;
 	}
-	
-	//13. Run the test again to see if you are finished.
+
+	// 13. Run the test again to see if you are finished.
 }
--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,18 +6,37 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
+		ArrayList<String> stringList = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		stringList.add("Hi");
+		stringList.add("Hello");
+		stringList.add("Hey");
+		stringList.add("Hiya");
+		stringList.add("Yo");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = 0; i<=stringList.size()-1; i++) {
+			System.out.println(stringList.get(i));
+		}
 		
 		//4. Print all the Strings using a for-each loop
-		
+		for(String s : stringList) {
+			System.out.println(s);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for(int i = 0; i<=stringList.size()-1; i++) {
+			if(i%2==0) {
+				System.out.println(stringList.get(i));
+			}
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for(int i = stringList.size()-1;i>=0; i-- ) {
+			System.out.println(stringList.get(i));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for(int i = 0; i<=stringList.size()-1; i++) {
+			if(stringList.get(i).contains("e")) {
+				System.out.println(stringList.get(i));
+			}
+		}
 	}
 }
