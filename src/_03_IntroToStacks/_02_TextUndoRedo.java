81ec3ff66045dc18b502734db37cfe46317343ec
root <root@bb284c7a27f0>
2020-08-19 01:23:59 +0000

    Finished Text Undo Redo and Test Matching Brackets

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -3,6 +3,7 @@ package _03_IntroToStacks;
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,72 +14,90 @@ public class _02_TextUndoRedo implements KeyListener {
 	JPanel pan = new JPanel();
 	JLabel lab = new JLabel();
 	char recentChar;
-	
-	 _02_TextUndoRedo() {
+	String typed = "";
+	Stack<String> deletedLetters = new Stack<String>();
+	int newLines = 1;
+	boolean HiTMaLls = false;
+
+	_02_TextUndoRedo() {
 		Initiate();
 	}
-	 
-	 void Initiate() {
-		 fra.add(pan);
-		 pan.add(lab);
-		 fra.addKeyListener(this);
-		 fra.setVisible(true);
-		 fra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 fra.setPreferredSize(new Dimension(500, 500));
-		 fra.pack();
-		 
-	 }
-	 
-	 void Add() {
-		 if(recentChar==KeyEvent.VK_BACK_SPACE) {
-			  Delete();
-		 }
-		 if(recentChar==KeyEvent.VK_LEFT) {
-			 Undo()
-		 }
-		 else {
-			 
-		 }
-	 }
-	 
-	 void Delete() {
-		 
-	 }
-	 
-	 void Undo() {
-		 
-	 }
-	/* 
+
+	void Initiate() {
+		fra.add(pan);
+		pan.add(lab);
+		fra.addKeyListener(this);
+		fra.setVisible(true);
+		fra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fra.setPreferredSize(new Dimension(500, 500));
+		fra.pack();
+
+	}
+
+	void Add(int keyCode) {
+		if (keyCode == KeyEvent.VK_BACK_SPACE) {
+			Delete();
+		} else if (keyCode == KeyEvent.VK_LEFT) {
+			Undo();
+		} else {
+			typed = typed + recentChar;
+
+			// typed = "<html>" + typed + "</html>"
+			// <br/>
+
+			if (typed.length() * 20 > 500 * newLines) {
+				typed = typed + "<br/>";
+				newLines += 1;
+			}
+
+			lab.setText("<html>" + typed + "</html>");
+		}
+	}
+
+	void Delete() {
+		deletedLetters.push(typed.substring(typed.length() - 1));
+		typed = typed.substring(0, typed.length()-1);
+		lab.setText("<html>" + typed + "</html>");
+	}
+
+	void Undo() {
+		if (!deletedLetters.isEmpty()) {
+			typed = typed + deletedLetters.pop();
+		}
+		lab.setText("<html>" + typed + "</html>");
+	}
+	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 * Every time a key is pressed, add that character to the JLabel. It should look
+	 * like a basic text editor.
 	 * 
-	 * Make it so that every time tshe BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
+	 * Make it so that every time the BACKSPACE key is pressed, the last character
+	 * is erased from the JLabel. Save that deleted character onto a Stack of
+	 * Characters.
 	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
+	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
+	 * the top Character is popped off the Stack and added back to the JLabel.
 	 * 
-	 * */
+	 */
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		recentChar =  arg0.getKeyChar();
-		Add();
+		recentChar = arg0.getKeyChar();
+		Add(arg0.getKeyCode());
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
 }
--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -20,30 +20,36 @@ public class _03_TestMatchingBrackets {
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String brackets) {
-		
+		Stack<Character> chars = new Stack<Character>();
 		//1. Use a for loop to iterate through your brackets String 
-
+		for(int i = 0; i < brackets.length(); i++) {
     			//2.  If the current character is an '{'
-
+			if(brackets.charAt(i)=='{') {
         			//3.  Push an '{' onto the stack 
-
+				chars.push('{');
+			}
     			//4.  else if the character is a '}'
-
+			else if(brackets.charAt(i)=='}') {
         			//6.  if the stack is empty (i.e. there is no matching opening bracket)
-
+				if(chars.isEmpty()) {
             				//7.  return false 
-
+					return false;
+				}
         			//5.  Pop a character from the stack
-
+				chars.pop();
+			}
+		}
 
 		//8.  if the stack is not empty (i.e. there were too many opening brackets)
-
+		if(!chars.isEmpty()) {
     			//9. return false 
-
+			return false;
+		}
 		//10. else (i.e. everything matched correctly)
-
+		else {
     			//11. return true 
 			return true;
+		}
 		
 	}
 
